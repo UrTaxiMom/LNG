caa9b904a213ef2f7a82c4b37c557e661e5a9467
chenyx <karta15626899@gmail.com>
2021-12-22 01:57:27 +0800

    Initial Commit

---
+++ random/randomApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomApp.swift
+//  random
+//
+//  Created by 陳以晢 on 2021/12/22.
+//
+
+import SwiftUI
+
+@main
+struct randomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
